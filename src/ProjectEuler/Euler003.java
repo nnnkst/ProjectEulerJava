47e0d52aaeb8571cdff0ab010994a93360decd47
Project Euler <nnnkst@gmail.com>
2023-06-12 00:34:17 +0530

    Project Euler Solutions using Java

---
+++ src/ProjectEuler/Euler003.java
@@ -0,0 +1,9 @@
+package ProjectEuler;
+import java.math.BigInteger;
+public class Euler003 {
+    public static void Solution()
+    {
+        BigInteger n = new BigInteger("600851475143");
+        System.out.println(n.sqrt());
+    }
+}
--- src/ProjectEuler/Main.java
+++ src/ProjectEuler/Main.java
@@ -2,9 +2,9 @@ package ProjectEuler;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print(Euler001.Solution());
-        System.out.println(Utilities.hcf(5,6));
-        Euler002.Solution();
+//        System.out.print(Euler002.Solution());
+//        System.out.println(Utilities.hcf(5,6));
+        Euler003.Solution();
     }
 }
 
