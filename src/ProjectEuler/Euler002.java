e2ae4edd1df9386ac337f88680f3892564e21b43
Project Euler <nnnkst@gmail.com>
2023-06-12 00:34:44 +0530

    Project Euler Solutions using Java

--- src/ProjectEuler/Euler002.java
+++ src/ProjectEuler/Euler002.java
@@ -3,6 +3,20 @@ package ProjectEuler;
 public class Euler002 {
     public static void Solution()
     {
-        System.out.print("Solution to Euler 002");
+        int a = 1;
+        int b = 2;
+        int c = a + b ;
+        int sum = b;
+        while(c < 4000000)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            if(c%2 == 0) {
+                sum += c;
+            }
+        }
+//        return sum;
+        System.out.print(sum);
     }
 }
